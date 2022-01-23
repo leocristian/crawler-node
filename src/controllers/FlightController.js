603ffb8f958b2885b36f47e00946b41ca2d7d693
leocristian <leonardosclopes@gmail.com>
2022-01-23 17:10:03 -0300

    add cheerio

--- src/controllers/FlightController.js
+++ src/controllers/FlightController.js
@@ -1,9 +1,25 @@
-
+const axios = require("axios")
 
 class FlightController {
     async get(req, res) {
         const { Origem, Destino, DataIda, DataVolta, Adultos, Criancas, Bebes } = req.body
 
-        
+        const data = {
+            Origem: Origem,
+            Destino: Destino,
+            DataIda: DataIda,
+            DataVolta: DataVolta,
+            Adultos: Adultos,
+            Criancas: Criancas,
+            Bebes: Bebes
+        }
+
+        const response = await axios.post("https://viajemais.voeazul.com.br/", data)
+
+        console.log(response)
+
+        return res.send(response.data)
     }
 }
+
+module.exports = new FlightController()
--- src/routes/flight.js
+++ src/routes/flight.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const router = express.Router()
 
-const flightController = require("./controllers/FlightController")
+const flightController = require("../controllers/FlightController")
 
 router.post("/flight", flightController.get)
 
